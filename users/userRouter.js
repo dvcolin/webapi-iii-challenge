c1e045b6d89b438fa0cd12f85f3c427ea91aa64a
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-04 12:57:21 -0700

    added get requests to /users. trying to fix error in getting user by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const UserDb = require('./userDb.js');
+
 router.post('/', (req, res) => {
 
 });
@@ -11,11 +13,25 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    UserDb.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'User data could not be accessed.' })
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+    const userBody = req.user;
 
+    UserDb.getById(userBody.id)
+    .then(user => {
+        res.status(200).json(userBody)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The user data cannot be accessed' })
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -33,6 +49,22 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const user = req.body;
+    const userId = req.params.id;
+
+    UserDb.getById(userId)
+    .then(result => {
+        if (result) {
+            req.user = user;
+        } else {
+            res.status(400).json({ message: "invalid user id" });
+        }
+
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 };
 
