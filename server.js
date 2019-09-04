977a61942fe0e9359f0a300b97400a7d27813615
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-04 12:17:08 -0700

    connected server and index files. set up express server and userRouter

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+const userRouter = require('./users/userRouter.js');
+
+server.use('/users', userRouter);
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
