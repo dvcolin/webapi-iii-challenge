a8f155430229cfb39493bfeb73c77e59e6fda8c7
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-04 12:27:21 -0700

    added custom logger middleware to server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const server = express();
 
 const userRouter = require('./users/userRouter.js');
 
+server.use(express.json());
+server.use(logger);
+
 server.use('/users', userRouter);
 
 
@@ -12,10 +15,17 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
+
+
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
 
+  next();
 };
 
 module.exports = server;
